21d021da5e95574e0022a04b508d36144fd0fc36
devkcud <andrescalisejr@gmail.com>
2023-02-01 12:42:38 -0300

    fix(Python): Fix scripts

--- src/pythonexec.rs
+++ src/pythonexec.rs
@@ -6,11 +6,7 @@ pub fn download_video_mp3(
     output_path: &str,
 ) -> Result<std::process::ExitStatus, std::io::Error> {
     Command::new(py_exec)
-        .args(["-c", "import sys, pytube
-try: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_audio_only().download(output_path=sys.argv[2], filename=f'{video_name}.mp3')
-except:
-    try: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_audio_only().download(output_path=sys.argv[2], filename=f'{video_name}.mp3')
-    except: exit(1)", url, output_path]).status()
+        .args(["-c", "import sys, pytube\ntry: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_audio_only().download(output_path=sys.argv[2], filename=f'{video_name}.mp3')\nexcept:\n\ttry: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_audio_only().download(output_path=sys.argv[2], filename=f'{video_name}.mp3')\n\texcept: exit(1)", url, output_path]).status()
 }
 
 pub fn download_video_mp4(
@@ -19,11 +15,7 @@ pub fn download_video_mp4(
     output_path: &str,
 ) -> Result<std::process::ExitStatus, std::io::Error> {
     Command::new(py_exec)
-        .args(["-c", "import sys, pytube
-try: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_highest_resolution().download(output_path=sys.argv[2], filename=f'{video_name}.mp4')
-except:
-    try: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_highest_resolution().download(output_path=sys.argv[2], filename=f'{video_name}.mp4')
-    except: exit(1)", url, output_path]).status()
+        .args(["-c", "import sys, pytube\ntry: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_highest_resolution().download(output_path=sys.argv[2], filename=f'{video_name}.mp4')\nexcept:\n\ttry: yt_obj = pytube.YouTube(sys.argv[1]); video_name = yt_obj.title.title().replace(' ', '').replace('!', '').replace('?', '').replace('/', '').replace(',', '').replace('.', ''); yt_obj.streams.get_highest_resolution().download(output_path=sys.argv[2], filename=f'{video_name}.mp4')\n\texcept: exit(1)", url, output_path]).status()
 }
 
 pub fn load_playlist(py_exec: &str, url: &str) -> Result<std::process::Output, std::io::Error> {
